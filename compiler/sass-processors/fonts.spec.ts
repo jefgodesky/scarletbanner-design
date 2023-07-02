df1dfd5eb1e5553df1528a1dfd550020fdd73293
Jason Godesky <jefgodesky@gmail.com>
2023-07-02 14:11:57 -0400

    fontProcessor: Fix quotes in test

--- compiler/sass-processors/fonts.spec.ts
+++ compiler/sass-processors/fonts.spec.ts
@@ -3,7 +3,7 @@ import fontProcessor from './fonts.js'
 
 describe('fontProcessor', () => {
   it('returns a string defining a Sass variable', () => {
-    const actual = fontProcessor('test', { stack: "'Helvetica', sans-serif" })
-    expect(actual).to.equal("$test: 'Helvetica', sans-serif;")
+    const actual = fontProcessor('test', { stack: '\'Helvetica\', sans-serif' })
+    expect(actual).to.equal('$test: \'Helvetica\', sans-serif;')
   })
 })
