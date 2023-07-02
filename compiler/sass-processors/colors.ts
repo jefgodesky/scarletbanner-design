7ff1edbe8098deaa7a4768132b6396f223afb29e
Jason Godesky <jefgodesky@gmail.com>
2023-07-02 15:44:23 -0400

    isColorData: Add type guard for ColorData
    fix

--- compiler/sass-processors/colors.ts
+++ compiler/sass-processors/colors.ts
@@ -7,5 +7,10 @@ const colorProcessor = (key: string, data: ColorData): string => {
   return `$${key}: ${data.hex};`
 }
 
+const isColorData = (obj: any): obj is ColorData => {
+  if (typeof obj !== 'object' || obj === null) return false
+  return obj.hex !== undefined && typeof obj.hex === 'string'
+}
+
 export default colorProcessor
-export { ColorData }
+export { ColorData, isColorData }
