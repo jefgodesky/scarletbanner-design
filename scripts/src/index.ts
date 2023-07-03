839278f8ac721c82118f60b03efc20746d419382
Jason Godesky <jefgodesky@gmail.com>
2023-07-03 14:20:44 -0400

    Fix how we use the document

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -2,8 +2,5 @@
 
 import Prism from 'prismjs'
 
-const document = document !== undefined ? document : null
-Prism.plugins.autoloader.languages_path = document !== null
-  ? document.currentScript.src.replace(/\/([^\/]+)$/,'/prism/components/')
-  : null
+Prism.plugins.autoloader.languages_path = document.currentScript.src.replace(/\/([^/]+)$/, '/prism/components/')
 Prism.highlightAll()
