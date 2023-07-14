92fb57e33f5891d1385a07e4614c4b1feb4bced7
Jason Godesky <jefgodesky@gmail.com>
2023-07-14 16:18:28 -0400

    renderTags: Pass optional data, call renderSwatches if it's provided

--- compiler/tags/render.spec.ts
+++ compiler/tags/render.spec.ts
@@ -10,7 +10,7 @@ describe('renderTags', () => {
       '{{ greeting.punctuation }}': '!'
     }
     const expected = 'Hello, world!'
-    const actual = renderTags(src, tags)
+    const actual = renderTags(src, tags, {})
     expect(actual).to.equal(expected)
   })
 
@@ -24,5 +24,14 @@ describe('renderTags', () => {
     const src = '{{ tag }} {{ tag }} {{ tag }}'
     const actual = renderTags(src, { '{{ tag }}': 'test' })
     expect(actual).to.equal('test test test')
+  })
+
+  it('renders swatches', () => {
+    const black = { name: 'Black', hex: '#000' }
+    const white = { name: 'White', hex: '#fff' }
+    const color = { black, white }
+    const src = '{{ swatches }}'
+    const actual = renderTags(src, {}, { color })
+    expect(actual).to.equal('<ul class="swatches"><li id="swatch-black" style="background: #000;"><h4>Black</h4><table><tbody><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGB</th><td>0, 0, 0</td></tr><tr><th>CMYK</th><td>0, 0, 0, 100</td></tr><tr><th>HSL</th><td>0&deg;, 0, 0</td></tr><tr><th>HSV</th><td>0&deg;, 0, 0</td></tr></tbody></table></li><li id="swatch-white" style="background: #fff;"><h4>White</h4><table><tbody><tr><th>Hex</th><td>#ffffff</td></tr><tr><th>RGB</th><td>255, 255, 255</td></tr><tr><th>CMYK</th><td>0, 0, 0, 0</td></tr><tr><th>HSL</th><td>0&deg;, 0, 100</td></tr><tr><th>HSV</th><td>0&deg;, 0, 100</td></tr></tbody></table></li></ul>')
   })
 })
--- compiler/tags/render.ts
+++ compiler/tags/render.ts
@@ -1,8 +1,12 @@
-const renderTags = (orig: string, tags: { [tag: string]: any }): string => {
+import renderSwatches from './swatches.js'
+
+const renderTags = (orig: string, tags: { [tag: string]: any }, data?: { [key: string]: any }): string => {
   let working = (' ' + orig).slice(1)
   for (const tag in tags) {
     working = working.replaceAll(tag, tags[tag])
   }
+
+  if (data !== undefined) working = working.replaceAll('{{ swatches }}', renderSwatches(data))
   return working
 }
 
