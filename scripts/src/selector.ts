7b13e76db6d5213d65a3dda036db05ade22f74df
Jason Godesky <jefgodesky@gmail.com>
2023-07-29 14:07:20 -0400

    Client-side: OK, let's start over...

--- scripts/src/selector.ts
+++
@@ -1,6 +0,0 @@
-const selector = (def: string): Element[] | null => {
-  const elems = document.querySelectorAll(def)
-  return elems.length === 0 ? null : Array.from(elems)
-}
-
-export default selector
