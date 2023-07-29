663fca09588a350f9b7a34552f85ea3ceb1b05df
Jason Godesky <jefgodesky@gmail.com>
2023-07-29 16:08:53 -0400

    Add new method: getOptions

---
+++ scripts/src/component-navigator/options.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+import jsdomGlobal from 'jsdom-global'
+import { create } from 'unobtrusive-dom'
+import getOptions from './options.js'
+
+describe('getOptions', () => {
+  let jsdomCleanUp: () => void
+
+  beforeEach(() => {
+    jsdomCleanUp = jsdomGlobal()
+    document.body.innerHTML = '<section id="a" data-component-category="Category A"><h2>Component A</h2></section><section id="b" data-component-category="Category B"><h2>Component B</h2></section>'
+  })
+
+  afterEach(() => {
+    jsdomCleanUp()
+  })
+
+  it('returns an array of options for the sections given', () => {
+    const options = getOptions([...document.querySelectorAll('section')])
+    const select = create({ tag: 'select' })
+    for (const option of options) select.appendChild(option)
+    expect(select.innerHTML).to.equal('<option value="a">Component A</option><option value="b">Component B</option>')
+  })
+})
