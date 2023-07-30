6e8bf1b8f87e0fdbd177b9c56f74512880da3fcc
Jason Godesky <jefgodesky@gmail.com>
2023-07-29 22:26:47 -0400

    getOptions: Add event listener to select, not option

--- scripts/src/component-navigator/options.ts
+++ scripts/src/component-navigator/options.ts
@@ -1,14 +1,11 @@
 import { create } from 'unobtrusive-dom'
-import handleChange from './handle-change.js'
 
 const getOptions = (components: Element[]): HTMLOptionElement[] => {
   return components.map(component => {
     const heading = component.querySelector('h2')
     const id = component.getAttribute('id') ?? 'Unknown'
     const text = heading?.innerHTML ?? id
-    const option = create({ tag: 'option', attrs: { value: id }, text }) as HTMLOptionElement
-    option.addEventListener('change', handleChange)
-    return option
+    return create({ tag: 'option', attrs: { value: id }, text }) as HTMLOptionElement
   })
 }
 
