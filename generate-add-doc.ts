4fb8224cc6e25b4c8cc40d5ffc500e056c694a0d
Jason Godesky <jefgodesky@gmail.com>
2023-09-03 09:56:51 -0400

    Fix links on homepage

--- generate-add-doc.ts
+++ generate-add-doc.ts
@@ -3,7 +3,8 @@ import pkg from './package.json' assert { type: 'json' }
 
 const generateAddDoc = (): void => {
   const add = {
-    version: pkg.version
+    version: pkg.version,
+    'version path': `v${pkg.version.split('.').join('/')}`
   }
   writeFileSync('./add-doc.json', JSON.stringify(add, null, 2), { encoding: 'utf8' })
 }
