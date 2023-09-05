6a7ed83ce2c68e39239297fb2f73b2acba24f6ad
Jason Godesky <jefgodesky@gmail.com>
2023-09-05 12:24:44 -0400

    Theme picker: getAngle

--- scripts/src/theme-picker/get-angle.spec.ts
+++ scripts/src/theme-picker/get-angle.spec.ts
@@ -6,12 +6,37 @@ describe('getAngle', () => {
     expect(getAngle('day')).to.equal(0)
   })
 
-  it('returns 180 if given night', () => {
-    expect(getAngle('night')).to.equal(180)
+  it('returns -180 if given night', () => {
+    expect(getAngle('night')).to.equal(-180)
   })
 
   it('returns the angle of the sun if given natural', () => {
-    expect(getAngle('natural', new Date(0), 0, 0)).to.equal(-66.94817090034745)
+    const lat = 51.4769
+    const lon = 0
+
+    const lastSunset = 1679249462529
+    const sunrise = 1679292306723
+    const sunset = 1679335963230
+
+    const lastNightLen = 42844194
+    const dayLen = 43656507
+    const nextNightLen = 42605699
+
+    const tests = [
+      { time: lastSunset + Math.round(lastNightLen * 0.75), min: 130, max: 140 },
+      { time: sunrise, min: 85, max: 95 },
+      { time: sunrise + Math.round(dayLen * 0.25), min: 40, max: 50 },
+      { time: sunrise + Math.round(dayLen * 0.5), min: -5, max: 5 },
+      { time: sunrise + Math.round(dayLen * 0.75), min: -50, max: -40 },
+      { time: sunset, min: -95, max: -85 },
+      { time: sunset + Math.round(nextNightLen * 0.25), min: -140, max: -130 }
+    ]
+
+    for (const test of tests) {
+      const angle = getAngle('natural', new Date(test.time), lat, lon)
+      expect(angle).to.be.greaterThan(test.min)
+      expect(angle).to.be.lessThan(test.max)
+    }
   })
 
   it('returns 0 if given natural but no time', () => {
--- scripts/src/theme-picker/get-angle.ts
+++ scripts/src/theme-picker/get-angle.ts
@@ -2,14 +2,21 @@ import { ThemePreference } from './get-preference'
 import SunCalc from 'suncalc'
 
 const getAngle = (pref: ThemePreference, time?: Date, latitude?: number, longitude?: number): number => {
-  const angles = {
-    day: 0,
-    night: 180,
-    natural: time !== undefined && latitude !== undefined && longitude !== undefined
-      ? SunCalc.getPosition(time, latitude, longitude).altitude * (180 / Math.PI)
-      : 0
-  }
-  return angles[pref]
+  if (pref === 'day') return 0
+  if (pref === 'night') return -180
+  if (time === undefined || latitude === undefined || longitude === undefined) return 0
+
+  const timestamp = time.getTime()
+  const dayLen = 24 * 60 * 60 * 1000
+  const times = SunCalc.getTimes(time, latitude, longitude)
+  const sunrise = times.sunrise.getTime()
+  const sunset = times.sunset.getTime()
+  const lastSunset = SunCalc.getTimes(new Date(timestamp - dayLen), latitude, longitude).sunset.getTime()
+  const nextSunrise = SunCalc.getTimes(new Date(timestamp + dayLen), latitude, longitude).sunrise.getTime()
+  const start = Math.round((sunrise + lastSunset) / 2)
+  const end = Math.round((nextSunrise + sunset) / 2)
+  const len = end - start
+  return (((timestamp - start) / len) * -360) + 180
 }
 
 export default getAngle
