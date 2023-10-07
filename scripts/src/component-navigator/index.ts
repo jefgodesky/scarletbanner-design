1e44bf7492a05edf2df0fb7f6b3c39e2fe4efda6
Jason Godesky <jefgodesky@gmail.com>
2023-10-07 15:55:29 -0400

    initComponentNavigator: Remove debugging console

--- scripts/src/component-navigator/index.ts
+++ scripts/src/component-navigator/index.ts
@@ -2,7 +2,6 @@ import getComponentNavigator from './nav'
 
 const initComponentNavigator = (components: HTMLElement[]): void => {
   const root = document.getElementById('component-navigator')
-  console.log(root)
   if (root === null) return
   const select = getComponentNavigator(components)
   root.appendChild(select)
