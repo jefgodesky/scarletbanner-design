12b2c0b28fca290caf18fbe4812a27a95f28ad4e
Jason Godesky <jefgodesky@gmail.com>
2023-10-07 15:01:55 -0400

    getVersionPath: Return path and version

--- release/get-version-path.spec.ts
+++ release/get-version-path.spec.ts
@@ -3,10 +3,14 @@ import getVersionPath from './get-version-path'
 
 describe('getVersion', () => {
   it('turns the version into a path', () => {
-    expect(getVersionPath('1.0.0')).to.equal('/v1/0/0')
+    expect(getVersionPath('1.0.0')[0]).to.equal('/v1/0/0')
+  })
+
+  it('returns the version', () => {
+    expect(getVersionPath('1.0.0')[1]).to.equal('1.0.0')
   })
 
   it('returns the version from package.json by default', () => {
-    expect(getVersionPath().startsWith('/v')).to.equal(true)
+    expect(getVersionPath().startsWith('/v')[0]).to.equal(true)
   })
 })
--- release/get-version-path.ts
+++ release/get-version-path.ts
@@ -1,13 +1,13 @@
 import fs from 'fs'
 
-const getVersionPath = (v?: string): string => {
+const getVersionPath = (v?: string): [string, string] => {
   if (v === undefined) {
     const pkgdata = fs.readFileSync('./package.json', { encoding: 'utf-8' })
     const pkg = JSON.parse(pkgdata)
     v = pkg.version as string
   }
 
-  return `/v${v.split('.').join('/')}`
+  return [`/v${v.split('.').join('/')}`, v]
 }
 
 export default getVersionPath
