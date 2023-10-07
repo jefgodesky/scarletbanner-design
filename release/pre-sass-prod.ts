c053e5f02c0478bbb4075daac21d6f2f8a1e11fd
Jason Godesky <jefgodesky@gmail.com>
2023-10-07 16:02:47 -0400

    Add presass:prod script

---
+++ release/pre-sass-prod.ts
@@ -0,0 +1,5 @@
+import getVersionPath from './get-version-path.js'
+import setBase from './set-base.js'
+
+const [base] = getVersionPath()
+setBase(base)
--- release/set-base.ts
+++ release/set-base.ts
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs'
 
-const setBase = (base: string, filename: string = '../stylesheets/src/modules/_paths.scss'): void => {
+const setBase = (base: string, filename: string = 'stylesheets/src/modules/_paths.scss'): void => {
   const original = readFileSync(filename, { encoding: 'utf-8' })
   const update = original.replace(/\$base: "(.*?)";/, `$base: "${base}";`)
   writeFileSync(filename, update, { encoding: 'utf-8' })
