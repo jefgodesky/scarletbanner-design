086bb4db66b735bbd67e4325b456c372f5ed6057
Jason Godesky <jefgodesky@gmail.com>
2023-10-07 15:53:58 -0400

    Fix failing test

--- release/get-version-path.spec.ts
+++ release/get-version-path.spec.ts
@@ -3,14 +3,17 @@ import getVersionPath from './get-version-path'
 
 describe('getVersion', () => {
   it('turns the version into a path', () => {
-    expect(getVersionPath('1.0.0')[0]).to.equal('/v1/0/0')
+    const actual = getVersionPath('1.0.0')
+    expect(actual[0]).to.equal('/v1/0/0')
   })
 
   it('returns the version', () => {
-    expect(getVersionPath('1.0.0')[1]).to.equal('1.0.0')
+    const actual = getVersionPath('1.0.0')
+    expect(actual[1]).to.equal('1.0.0')
   })
 
   it('returns the version from package.json by default', () => {
-    expect(getVersionPath().startsWith('/v')[0]).to.equal(true)
+    const actual = getVersionPath()
+    expect(actual[0].startsWith('/v')).to.equal(true)
   })
 })
