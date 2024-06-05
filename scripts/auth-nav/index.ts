cc0b86c038071127f08e9f772d9ae91bfe8375d9
Jason Godesky <jefgodesky@gmail.com>
2024-06-05 12:56:45 -0400

    Prevent initial blip before JS loads
    We'll use a delayed animation to reveal the menu if JS doesn't load. When JS loads, it interrupts this and keeps the nav auth anchored to user interaction, This optimizes for the most likely case (JS loads) while still delivering something that works when JS doesn't.

--- scripts/auth-nav/index.ts
+++ scripts/auth-nav/index.ts
@@ -4,10 +4,21 @@ import handleToggle from './handle-toggle'
 const initAuthNav = (components: HTMLElement[]): void => {
   if (components.length < 1) return
   const header = components[0]
+
+  // Put the header into its starting position without transition to
+  // avoid a "blip" of the menu
+  header.setAttribute('style', 'transition: none;')
+
   addClass(header, 'initialized', 'closed')
   const toggle = create({ tag: 'span', classes: ['toggle'] })
   toggle.addEventListener('click', handleToggle)
   header.appendChild(toggle)
+
+  // After any time at all has passed, we can safely remove the
+  // style attribute we added before to prevent the blip.
+  window.setTimeout(() => {
+    header.removeAttribute('style')
+  }, 1)
 }
 
 export default initAuthNav
